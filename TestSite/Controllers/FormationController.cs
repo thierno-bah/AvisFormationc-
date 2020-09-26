02f603078c46df73ca7dd165a9ccf66c385cd471
thierno-bah <54667738+thierno-bah@users.noreply.github.com>
2020-09-26 04:46:50 -0400

    commit mon testC#project

---
+++ TestSite/Controllers/FormationController.cs
@@ -0,0 +1,26 @@
+﻿using AvisFormation.data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestSite.Controllers
+{
+    public class FormationController : Controller
+    {
+        // GET: Formation
+        public ActionResult Index()
+        {
+            List<Formation> ListFormations = new List<Formation>();
+            using (var context = new AvisTestDbEntities())
+            {
+
+                 ListFormations = context.Formations.ToList();
+            
+            
+            };
+            return View(ListFormations);
+        }
+    }
+}
---
+++ TestSite/Controllers/TotoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestSite.Controllers
+{
+    public class TotoController : Controller
+    {
+        // GET: Toto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
